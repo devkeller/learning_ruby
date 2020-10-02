1d9160fe6ee17da1ef9f029495f43e519da68d7c
Karl Keller <karl.keller@outlook.com>
2020-10-02 21:08:56 +0200

    inlined case statements

--- oldschool_ui.rb
+++ oldschool_ui.rb
@@ -2,14 +2,11 @@
 puts "What do you want to do? [read|write|exit]"
 action = gets.chomp
 
-# case statement
+# inlined case statement
 case action
-when "read"
-  puts "Entering read mode..."
-when "write"
-  puts "Entering write mode..."
-when "exit"
-  puts "Goodbye!"
+when "read"   then puts "Entering read mode..."
+when "write"  then puts "Entering write mode..."
+when "exit"   then puts "Goodbye!"
 else
   puts "Incorrect choice! -check your spelling"
 end
