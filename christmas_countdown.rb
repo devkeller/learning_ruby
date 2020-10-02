ef492234b3c4fef13ff5ef4d63e2299712eebb49
Karl Keller <karl.keller@outlook.com>
2020-10-02 02:43:36 +0200

    Passing rudimentray tests

--- christmas_countdown.rb
+++ christmas_countdown.rb
@@ -4,7 +4,9 @@ def countdown_to_xmas
   # 1. What day is it today?
   today = Date.today
   # 2. When is the next christmas?
+  christmas = Date.new(2020, 12, 24)
   # 3. Subtract today from christmas
+  return (christmas - today).to_i
 end
 
 # Rudimentray testing
