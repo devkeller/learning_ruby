7b359ae86d250cace5f0bcd51909d732cfdbefd8
Karl Keller <karl.keller@outlook.com>
2020-10-02 02:42:27 +0200

    Add today'S date

--- christmas_countdown.rb
+++ christmas_countdown.rb
@@ -1,9 +1,12 @@
+require 'date'
+
 def countdown_to_xmas
   # 1. What day is it today?
+  today = Date.today
   # 2. When is the next christmas?
   # 3. Subtract today from christmas
 end
 
 # Rudimentray testing
-p countdown_to_xmas.class == Integer
-p countdown_to_xmas == 83
+puts countdown_to_xmas.class == Integer
+puts countdown_to_xmas == 83
