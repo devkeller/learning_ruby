8c48217cc973fe6431539ffdb41c6864a3b2d95d
Karl Keller <karl.keller@outlook.com>
2020-10-02 02:41:17 +0200

    Rudimentray testing with print statements

---
+++ christmas_countdown.rb
@@ -0,0 +1,9 @@
+def countdown_to_xmas
+  # 1. What day is it today?
+  # 2. When is the next christmas?
+  # 3. Subtract today from christmas
+end
+
+# Rudimentray testing
+p countdown_to_xmas.class == Integer
+p countdown_to_xmas == 83
