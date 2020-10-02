231b7fbfccee3b69501bd1d0354d0c5fd8be5e1b
Karl Keller <karl.keller@outlook.com>
2020-10-02 03:00:44 +0200

    Now accurate if run after christmas or in another year

--- christmas_countdown.rb
+++ christmas_countdown.rb
@@ -1,14 +1,21 @@
 require 'date'
 
 def christmas_countdown
-  # 1. What day is it today?
+  # Get today's date and year
   today = Date.today
-  # 2. When is the next christmas?
-  christmas = Date.new(2020, 12, 24)
-  # 3. Subtract today from christmas
+  year = today.year
+
+  # Check if it is after christmas this year?
+  if today.month == 12 && today.day > 24
+    year +=1
+  end
+
+  christmas = Date.new(year, 12, 24)
+  # Subtract today from christmas and convert 
+  # resulting Rational to Integer
   return (christmas - today).to_i
 end
 
 # Rudimentray testing
-puts christmas_countdown.class == Integer
-puts christmas_countdown == 83
+p christmas_countdown.class == Integer
+p christmas_countdown == 83
