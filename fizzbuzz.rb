a0222174cb1fddc75f636e80c01eea437388ea4a
Karl Keller <karl.keller@outlook.com>
2020-10-07 03:17:50 +0200

    Added 5 Specifictaions for new method #fizz_buzz_array, that returns an array.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,6 +12,9 @@ class FizzBuzz
       return number.to_s
     end
   end
+
+  def fizz_buzz_array(number)
+  end
 end
 
 
@@ -51,5 +54,28 @@ describe FizzBuzz do
     it "should return '#{result}' for #{input}" do
       expect(subject.fizz_buzz(input)).to eq(result)
     end
+  end
+end
+
+# Specification for #fizz_buzz_array which returns an array
+describe FizzBuzz do
+  it "should raise an argument error if passed a number below 1" do
+    expect { subject.fizz_buzz_array(0) }.to raise_error ArgumentError
+  end
+
+  it "should return the array [ 1 ] for number 1" do
+    expect(subject.fizz_buzz_array(1)).to eq([ 1 ])
+  end
+
+  it "should return the array [ 1, 2, 'Fizz' ] for number 3" do
+    expect(subject.fizz_buzz_array(3)).to eq([ 1, 2, 'Fizz'])
+  end
+
+  it "should return the array [ 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7 ] for number 7" do
+    expect(subject.fizz_buzz_array(7)).to eq([ 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7 ])
+  end
+
+  it "should return an array with 'FizzBuzz' at the 15th element of the array (15 is divisible by both 3 and 5)" do
+    expect(subject.fizz_buzz_array(100)[14]).to eq 'FizzBuzz'
   end
 end
