8e7a3cd2092fc589d7f867628654c3e3ca7d7368
Karl Keller <karl.keller@outlook.com>
2020-10-10 00:28:16 +0200

    Blocks and Yield: Learning how to use blocks and yield properly. A block is like an anonymous method, i.e. it has no name, an nonymous collection of code.

--- blocks.rb
+++ blocks.rb
@@ -1,5 +1,20 @@
 # Blocks and Yield
 
+# Blocks are like anonymous methods. They are a anonymous collection of code.
+# examples
+# { |block variable(s)| code}
+# 
+# do |block variable(s)| 
+#   code 
+# end
+
+def execute_block
+  #block_given?
+  yield
+end
+
+execute_block { puts "Help, I am being executed!" }
+
 # How would you define a method accepting a block with dynamic parameters?
 def greet(name)
   capitalized_name = name.capitalize
