dfe5440978db1152a30cea1e558f1d4e51c6054b
Karl Keller <karl.keller@outlook.com>
2020-10-11 22:40:10 +0200

    Added user prompts and used interpolation instead of concatenation

--- 30.days.of.code/Day2_Operators.rb
+++ 30.days.of.code/Day2_Operators.rb
@@ -1,4 +1,9 @@
+puts "Enter price of meal"
+mealCost = gets.chomp.to_f
+puts "Enter amount to tip"
+tipPercent = gets.chomp.to_i
+puts "Enter VAT as a number"
 taxPercent = gets.chomp.to_i
 
 totalCost = mealCost + (mealCost*(tipPercent.to_f/100)) + (mealCost*(taxPercent.to_f/100))
-puts "The total meal cost is " + totalCost.round.to_s + " dollars." 
+puts "The total meal cost is #{totalCost.round.to_s} dollars."
