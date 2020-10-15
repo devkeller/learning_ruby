be766575c94f722b122f95afc29b160feabd4cee
Karl Keller <karl.keller@outlook.com>
2020-10-15 03:01:07 +0200

    Matching a phone number and querying MatchData; Matching e-mail address with named captures

--- matching_examples.rb
+++ matching_examples.rb
@@ -17,3 +17,21 @@ end
 puts "Here's another way to get at the first capture: "
 print "Capture #1: "
 puts match[1]
+
+
+# Example of Named Captures
+# Method returns a Hash with name, domain and tld extracted from email
+def compose_mail(email)
+  # Catures named in <capture_name>
+  groups = email.match(/^(?<username>.+)@(?<domain>.+)\.(?<tld>\w+)$/)
+  {
+    username: groups[:username],
+    domain: groups[:domain],
+    tld: groups[:tld]
+  }
+end
+
+extracted_hash = compose_mail("john555@devkeller.tech")
+extracted_hash. each do |key, value|
+  puts "#{key.to_s} = #{value}"
+end
