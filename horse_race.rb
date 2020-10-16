6cc7296803c5e3a1b7be3674c2dae7ca3125b3a0
Karl Keller <karl.keller@outlook.com>
2020-10-16 23:44:27 +0200

    Horse Race - fun Array data type practice.

--- horse_race.rb
+++ horse_race.rb
@@ -15,9 +15,15 @@ puts "You chose #{horses[bet_index]}"
 # Begin the race and choose a horse at random to win
 puts "The race is about to begin!"
 sleep(1)
-puts "#{horses.sample} is in the lead!"
+first_announcement = horses.sample
+puts "#{first_announcement} is in the lead!"
 sleep(1)
-puts "Now #{horses.sample} is in the lead!"
+second_announcement = horses.sample
+if first_announcement == second_announcement
+  puts "#{first_announcement} is dominating this race!"
+else
+  puts "Now #{second_announcement} is in the lead!"
+end
 sleep(1)
 puts "The race is done!"
 winning_horse_index = rand(0...horses.length)
