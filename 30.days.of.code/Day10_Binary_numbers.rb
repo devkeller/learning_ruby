c3c1c964ae71a1d494a752e268d521b86c1e04d2
Karl Keller <karl.keller@outlook.com>
2020-11-06 02:45:51 +0100

    Convert a decimal 'n' to binary, then print the decimal integer denoting the maximum number of consecutive 1's in n's binary representation.

--- 30.days.of.code/Day10_Binary_numbers.rb
+++ 30.days.of.code/Day10_Binary_numbers.rb
@@ -1,9 +1,11 @@
+# Constraint: n is a positive integer!
 n = gets.strip.to_i
 
-# Constraint: n is a positive integer!
+# Given a base-10 integer, n, convert it to binary (base-2).
 def to_binary(n)
   n.to_s(2)
 end
-
+# Then find and print the base-10 integer denoting the maximum
+# number of consecutive 1's in n's binary representation.
 array = to_binary(n).split.map(&:to_i)
 puts array.join.split("0").group_by(&:size).max.first
